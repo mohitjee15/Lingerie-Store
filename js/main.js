22bf9d649febfe54123ac4e273012bf545aa3098
Mohit Suman <mohitsuman15@gmail.com>
2014-04-16 15:08:58 +0530

    First commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    
+    //To toggle offcanvas trigger class
+    $("#offcanvas-trigger").click(function(){
+        $(this).toggleClass("off-canvas-toggle");
+    });
+    
+});
